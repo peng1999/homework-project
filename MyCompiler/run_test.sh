e0fbe0fc811892bac58bdd88059c595b3aa729b1
Peng Guanwen <pg999w@outlook.com>
2018-12-23 00:25:13 +0800

    Update run_test.sh

--- MyCompiler/run_test.sh
+++ MyCompiler/run_test.sh
@@ -3,9 +3,12 @@
 # Run this file, it will give all output
 tests=("2 + 3 / 23 * (|23 * |3 - -4|| + 4)"
        "a = 1\na + 1"
-       "sin(2 + 3) * |-4|")
+       "sin(2 + 3) * |-4|"
+       "fun d(x) = x * 2\na = 4\nd(a)")
 for t in $tests; do
-    echo "test with $t:"
+    echo "test with \n$t"
+    echo "------------"
     echo $t | ./cmake-build-debug/lang
     echo "--- done ---"
+    echo
 done
